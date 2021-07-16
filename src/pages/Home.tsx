f8bcd3d4d92fb60dc480f130484a3bc83bba6993
Ken Sodemann <ken@ionic.io>
2021-07-16 06:21:11 -0500

    feat(Home): start filling out the Home content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
 import {
   IonContent,
+  IonFab,
+  IonFabButton,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { add } from 'ionicons/icons';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -13,16 +22,29 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Tea Categories</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={add}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
+        <IonList>
+          <IonItemSliding>
+            <IonItem>
+              <IonLabel>
+                <div></div>
+                <div></div>
+              </IonLabel>
+            </IonItem>
+            <IonItemOptions>
+              <IonItemOption></IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
+        </IonList>
       </IonContent>
     </IonPage>
   );
